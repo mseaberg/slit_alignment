139b52c638d01b4ba35e3e88724ef1d5a925186e
Matthew H. Seaberg <seaberg@slac.stanford.edu>
2020-05-28 08:35:51 -0700

    gitignore fix

--- PPM_centroid/processing_module.py
+++ PPM_centroid/processing_module.py
@@ -15,89 +15,16 @@ class RunProcessing(QtCore.QObject):
     def __init__(self, imager_prefix, data_dict):
         super(RunProcessing, self).__init__()
 
-        # self.my_signal = QtCore.Signal()
-
-        # self.gui = gui
-
+        # PPM object for image acquisition and processing
         self.PPM_object = optics.PPM_Imager(imager_prefix, threshold=0.1)
-
-        # self.cam_name = imager_prefix + 'CAM:'
-        # self.epics_name = self.cam_name + 'IMAGE2:'
-        #
-        # # if len(sys.argv)>1:
-        # #     self.cam_name = sys.argv[1]
-        # #     self.epics_name = sys.argv[1] + 'IMAGE2:'
-        #
-        # self.image_pv = PV(self.epics_name + 'ArrayData')
-        #
-        # # get ROI info
-        # xmin = PV(self.epics_name + 'ROI:MinX_RBV').get()
-        # xmax = xmin + PV(self.epics_name + 'ROI:SizeX_RBV').get() - 1
-        # ymin = PV(self.epics_name + 'ROI:MinY_RBV').get()
-        # ymax = ymin + PV(self.epics_name + 'ROI:SizeY_RBV').get() - 1
-        # # get binning
-        # xbin = PV(self.epics_name + 'ROI:BinX_RBV').get()
-        # ybin = PV(self.epics_name + 'ROI:BinY_RBV').get()
-        # # get array size
-        # self.xsize = PV(self.epics_name + 'ROI:ArraySizeX_RBV').get()
-        # self.ysize = PV(self.epics_name + 'ROI:ArraySizeY_RBV').get()
-        #
-        # self.x1d = np.linspace(xmin, xmax - (xbin - 1), self.xsize)
-        # self.x1d -= (xmax+1)/2
-        # self.y1d = np.linspace(ymin, ymax - (ybin - 1), self.ysize)
-        # self.y1d -= (ymax+1)/2
-        # self.x, self.y = np.meshgrid(self.x1d, self.y1d)
-        #
-        # FOV_dict = {
-        #     'IM2K4': 8.5,
-        #     'IM3K4': 8.5,
-        #     'IM4K4': 5.0,
-        #     'IM5K4': 8.5,
-        #     'IM6K4': 8.5,
-        #     'IM1K1': 8.5,
-        #     'IM2K1': 8.5,
-        #     'IM1K2': 8.5,
-        #     'IM2K2': 18.5,
-        #     'IM3K2': 18.5,
-        #     'IM4K2': 8.5,
-        #     'IM5K2': 8.5,
-        #     'IM6K2': 5.0,
-        #     'IM7K2': 5.0,
-        #     'IM1L1': 8.5,
-        #     'IM2L1': 8.5,
-        #     'IM3L1': 8.5,
-        #     'IM4L1': 8.5,
-        #     'IM1K3': 8.5,
-        #     'IM2K3': 8.5,
-        #     'IM3K3': 8.5,
-        #     'IM3L0': 5.0
-        # }
-        #
-        # try:
-        #     self.distance = FOV_dict[self.epics_name[0:5]] * 1e3
-        # except:
-        #     self.distance = 8500.0
-
-        # try:
-        #     self.gige = PCDSAreaDetector(self.cam_name, name='gige')
-        #     self.reset_camera()
-        # except Exception:
-        #     print('\nSomething wrong with camera server')
-        #     self.gige = None
-
-        # self.connect(self.gui, QtCore.SIGNAL('stop()'), self.stop)
 
         self.running = True
 
-        # gui.img0.setImage(im1)
-
-        # self.counter = 0
+        # frame rate initialization
         self.fps = 0.
         self.lastupdate = time.time()
 
-        # self.contrast = np.zeros((4,100))
-        # self.iteration = np.tile(np.linspace(-99, 0, 100),(4,1))
-
+        # initialize dictionary to pass for plotting
         self.data_dict = data_dict
 
         self.counter = self.data_dict['counter']
@@ -108,21 +35,10 @@ class RunProcessing(QtCore.QObject):
     def _update(self):
 
         if self.running:
-
+            # get latest image
             self.PPM_object.get_image()
 
-            # self.im1 = np.ones((2048,2048))*255
-            # self.im1, time_stamp = self.get_image()
-            #
-            # cx, cy = self.get_centroids(self.im1)
-
-            # self.gui.img0.setImage(np.flipud(self.im1).T,levels=(0,300))
-
-            # self.data_dict['contrast'] = np.roll(self.data_dict['contrast'],-1,axis=1)
-            # self.data_dict['rotation'] = np.roll(self.data_dict['rotation'],-1,axis=1)
-            # self.data_dict['iteration'] = np.roll(self.data_dict['iteration'],-1,axis=1)
-            # self.data_dict['iteration'][:,-1] = self.counter
-
+            # update dictionary
             self.data_dict['cx'] = np.roll(self.data_dict['cx'], -1)
             self.data_dict['cy'] = np.roll(self.data_dict['cy'], -1)
             self.data_dict['timestamps'] = np.roll(self.data_dict['timestamps'], -1)
@@ -130,31 +46,18 @@ class RunProcessing(QtCore.QObject):
             self.data_dict['cy'][-1] = self.PPM_object.cy
             self.data_dict['timestamps'][-1] = self.PPM_object.time_stamp
 
-            # alignment_output = self.yag1.check_alignment(self.im1)
-
-            # self.data_dict['contrast'][:,-1] = alignment_output['contrast']
-
-            # lineout_x = np.sum(self.PPM_object.im1, axis=0)
-            # lineout_y = np.sum(self.im1, axis=1)
+            # get lineouts
             lineout_x = self.PPM_object.x_lineout
             lineout_y = self.PPM_object.y_lineout
 
+            # update dictionary
             self.data_dict['im1'] = self.PPM_object.profile
             self.data_dict['lineout_x'] = lineout_x/np.max(lineout_x)
             self.data_dict['lineout_y'] = lineout_y/np.max(lineout_y)
             self.data_dict['x'] = self.PPM_object.x
             self.data_dict['y'] = self.PPM_object.y
-            # translation = alignment_output['translation']
 
-            # centering_x = 1024 + np.sum(translation[:,1])/4
-            # centering_y = 1024 + np.sum(translation[:,0])/4
-            # self.data_dict['centering'] = np.array([centering_x,centering_y])
-
-            # scale
-            # centering
-            center = np.zeros((4, 2))
-            # center[0,:] = translation[0,:]*scale[0] + np.array([1792,256])
-
+            # frame rate code
             now = time.time()
             dt = (now - self.lastupdate)
             if dt <= 0:
@@ -165,8 +68,10 @@ class RunProcessing(QtCore.QObject):
             tx = 'Mean Frame Rate:  {fps:.3f} FPS'.format(fps=self.fps)
             self.data_dict['tx'] = tx
 
+            # send data
             self.sig.emit(self.data_dict)
 
+            # keep running unless the stop button is pressed
             if self.running:
                 QtCore.QTimer.singleShot(100, self._update)
                 self.counter += 1
