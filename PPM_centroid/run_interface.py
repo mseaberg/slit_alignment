69143dba7720c92969975435f1443aad9d17ca71
Matthew H. Seaberg <seaberg@slac.stanford.edu>
2020-10-19 12:29:33 -0700

    added epics plots, target position, etc

--- PPM_centroid/run_interface.py
+++ PPM_centroid/run_interface.py
@@ -2,11 +2,22 @@ from PPM_interface import PPM_Interface
 import warnings
 from pyqtgraph.Qt import QtGui
 import sys
+import argparse
+
+def process_cl_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--camera', action='store')
+
+    parsed_args, unparsed_args = parser.parse_known_args()
+    return parsed_args, unparsed_args
 
 if __name__ == '__main__':
 
+    parsed_args, unparsed_args = process_cl_args()
     warnings.filterwarnings('ignore', '.*output shape of zoom.*')
-    app = QtGui.QApplication(sys.argv)
-    thisapp = PPM_Interface()
+
+    qt_args = sys.argv[:1] + unparsed_args
+    app = QtGui.QApplication(qt_args)
+    thisapp = PPM_Interface(args=parsed_args)
     thisapp.show()
     sys.exit(app.exec_())
